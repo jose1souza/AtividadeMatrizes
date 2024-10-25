ce3bef983fa15e85d6928cb88f7d2b9372e44c4d
jose1souza <zexnineestudos@gmail.com>
2024-10-25 10:18:48 -0300

    versao1

---
+++ BibliotecaMatriz.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace BibliotecaMatriz { 
+    class Matrizes
+    {
+    public static void leia(int[,] mat)
+    {
+        int linhas = mat.GetLength(0);
+        int colunas = mat.GetLength(1);
+
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.WriteLine($"[{i},{j}]");
+                mat[i, j] = int.Parse(Console.ReadLine());
+            }// fim do for colunas
+        }// fim do for linhas
+    }
+    // ---------------------------------------- fim do leia
+    public static void mostra(int[,] mat)
+    {
+        int linhas = mat.GetLength(0);
+        int colunas = mat.GetLength(1);
+        for (int i = 0; i < linhas; i++)
+        {
+            for (int j = 0; j < colunas; j++)
+            {
+                Console.Write($"{mat[i, j],3} |");
+            } // fim do for colunas
+            Console.WriteLine();
+        }// fim do for linhas
+        // ------------------------------------------ fim do mostra
+    }
+        public static void gera(int[,] mat)
+        {
+            int linhas = mat.GetLength(0);
+            int colunas = mat.GetLength(1);
+
+            Random random = new Random();
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    mat[i,j] = random.Next(0, 100);
+                } // fim do for colunas
+
+                Console.WriteLine();
+
+            }// fim do for linhas
+
+        }// ------------------------------------------ fim do gera
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BibliotecaMatriz;
+using System.IO;
+class Program
+{
+     static void Main()
+    {
+        int linhas, colunas;
+
+        // tipo[,] nomedamatriz = new tipo {linha,coluna}
+
+        Console.WriteLine("*** Matrizes ***");
+
+        Console.WriteLine("Quantidade de linhas e colunas");
+
+        linhas = int.Parse(Console.ReadLine());
+        colunas = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[linhas, colunas]; // criando
+
+       // Console.WriteLine("Entre com dados para a matriz: ");
+        //Matrizes.leia(matrix);
+
+       // Console.WriteLine("Dados da matriz: ");
+        //Matrizes.mostra(matrix);
+
+        Console.WriteLine("Entre com os dados para a matriz");
+        Matrizes.gera(matrix);
+
+        Console.WriteLine("Dados da matriz: ");
+        Matrizes.mostra(matrix);
+
+        Console.ReadKey();
+    }
+}
